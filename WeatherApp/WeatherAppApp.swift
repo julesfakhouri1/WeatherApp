22ad1b505ee68d85377560feb84c672d317a6326
Jules Fakhouri <jules.fakhouri@qobuz.com>
2024-03-20 11:38:16 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jules Fakhouri on 20/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
